f17a98ad40172f461d09132bcd8380ceaed2f8bf
jondubya86 <jonathanwu86@gmail.com>
2016-10-06 07:54:36 -0400

    do not save

--- src/displaytea.js
+++ src/displaytea.js
@@ -15,31 +15,51 @@ var DisplayTea = React.createClass({
   },
   render: function() {
   	var arr = []
-  	if(this.props.params.category === 'black')
-  			{ 
-  				arr.push(this.state.menuList.black.map(
-  				a => <TeaItems category="black"image={a.image}name={a.name} price={a.price} description={a.description} ingredients={a.ingredients} />
-  			))}
-  	else if(this.props.params.category === 'green')
-  			{ 
-  				arr.push(this.state.menuList.green.map(
-  				a => <TeaItems category="green" image={a.image}name={a.name} price={a.price} description={a.description} ingredients={a.ingredients} />
-  			))}
-  	else if(this.props.params.category === 'herbal')
-  			{ 
-  				arr.push(this.state.menuList.herbal.map(
-  				a => <TeaItems category="herbal"image={a.image}name={a.name} price={a.price} description={a.description} ingredients={a.ingredients} />
-  			))}
-    return (
-      <div >
-  				<ul className="arrUl">	
-  					{arr}
-  				</ul>
+            if(this.props.params.name === 'black')
+        { 
+          arr.push(this.state.menuList.black.map(
+          a =>  
+
+          <div class="container">
+            <h1>Black Tea</h1>
+              <div class="row">
+                <div class="row-height">
+                  <div class="col-xs-4 col-lg-4" id="columns">
+                    <div class="inside inside-full-height">
+                      <div class="content">
+                        <h2><TeaItems name={a.name} /></h2>
+                        <p><TeaItems description={a.description}/></p>
+                        <a className="btn btn-default" href="#" role="button">
+                          <TeaItems price = {a.price}/> &raquo;</a>
+                      </div>
+                    </div>
+                  </div>
+                    <div class="col-xs-6 col-sm-3 col-sm-height col-sm-top">
+                      <div class="inside inside-full-height">
+                        <div class="content">
+                        </div>
+                      </div>
+                    </div>
+                      <div class="col-xs-6 col-sm-2 col-sm-height col-sm-middle">
+                        <div class="inside inside-full-height">
+                          <div class="content">
+                          </div>
+                        </div>
+                      </div>
+                        <div class="col-xs-6 col-sm-1 col-sm-height col-sm-bottom">
+                          <div class="inside inside-full-height">
+                            <div class="content">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>))}
+              return (
+      <div>
+          <ul className="arrUl">  
+            {arr}
+          </ul>
 
        </div>
-    )
-  }
-})
-
-
-export default DisplayTea;
+    )}}})
+export default DisplayTea
