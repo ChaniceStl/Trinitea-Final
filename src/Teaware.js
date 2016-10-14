e4454f97a59fe8dd14af56930176fc3afde3fabb
Quan <QuanAVuong@users.noreply.github.com>
2016-10-14 00:05:02 -0400

    added TeawareList

--- src/Teaware.js
+++ src/Teaware.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from "./data.js"
-import TeaItems from "./teaitem.js"
+import TeawareList from "./TeawareList.js"
 
 
 var Teaware = React.createClass({
@@ -18,21 +18,15 @@ var Teaware = React.createClass({
 
   render: function() {
     var arr = []
-    
-    if(this.props.params.product === 'BOCHA')
-        { 
-          arr.push(this.state.teawareList.herbal.map(
-          teaware => <TeaItems 
-          	category="herbal"
-          	image={a.image}
-          	name={a.name}
-          	price={a.price}
-          	description={a.description}
-          	ingredients={a.ingredients} />
-        ))}
+      arr.push(this.state.teawareList.map( teaware => <TeawareList 
+		  	image={teaware.image}
+		  	name={teaware.name}
+		  	price={teaware.price}
+		  	/>
+      ))
+
     return (
       <div >
-      		<h1> Teaware Page </h1>
           <ul className="arrUl">  
             {arr}
           </ul>
---
+++ src/TeawareList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from "react-router"
+
+var TeawareList = React.createClass({
+	render: function(){
+	var divStyle = {
+            backgroundImage: 'url(' + this.props.image + ')'
+        }
+	var bgStyle = {
+			backgroundImage: 'url(' + '"http://img.wallpaperfolder.com/f/5574BC310A55/tea-fields-images.jpg"' + ')'
+	}	
+		return(
+			<div style ={bgStyle}>
+				<div className = "teaColumns">
+        			<div className="col-lg-4 col-xs-4">
+          				<div className="panel panel-default">
+            				<div className="panel-heading">
+            				<h4>{this.props.name}</h4>
+            				</div>
+            				<div className="panel-body" style={divStyle}>
+            				</div>
+            				<Link to={"/Teaware/"+ this.props.name}><a  className="btn btn-default" href="#" role="button"><p>More information &raquo;</p></a></Link>
+          				</div>
+					</div>
+				</div>
+			</div>
+			)
+	}
+})
+
+export default TeawareList
